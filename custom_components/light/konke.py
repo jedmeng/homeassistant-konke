cbe388d295259ad628078192adbbc13a7f1ce871
Jedmeng <roy.urey@gmail.com>
2018-11-13 12:09:59 +0800

    update pykonkeio version

--- custom_components/light/konke.py
+++ custom_components/light/konke.py
@@ -23,7 +23,7 @@ from homeassistant.util.color import \
 from homeassistant.util.color import \
     color_RGB_to_hs as RGB_to_hs
 
-REQUIREMENTS = ['pykonkeio>=2.1.5']
+REQUIREMENTS = ['pykonkeio>=2.1.7']
 
 _LOGGER = logging.getLogger(__name__)
 
--- custom_components/switch/konke.py
+++ custom_components/switch/konke.py
@@ -14,7 +14,7 @@ from homeassistant.components.switch import SwitchDevice, PLATFORM_SCHEMA
 from homeassistant.const import CONF_NAME, CONF_HOST
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pykonkeio>=2.1.5']
+REQUIREMENTS = ['pykonkeio>=2.1.7']
 
 _LOGGER = logging.getLogger(__name__)
 
